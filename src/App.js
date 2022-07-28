49788743957cc83d1b9b02d44bd39f6739db579c
timothyrusso <russotimothy@live.it>
2022-07-28 07:45:25 +0200

    feat: installed axios

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
 
+// https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false
+
 function App() {
   return (
     <div className="App">
-      
+
     </div>
   );
 }

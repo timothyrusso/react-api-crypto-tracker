95024d28b74387b3ed6f3cb86772ec8f1bfe38ce
timothyrusso <russotimothy@live.it>
2022-07-29 07:46:45 +0200

    feat: axios get call

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
+import React, { useState, useEffect } from 'react';
 import './App.css';
-
-// https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false
+import axios from 'axios';
 
 function App() {
-  return (
-    <div className="App">
+  const [coins, setCoins] = useState([]);
 
-    </div>
-  );
+  useEffect(() => {
+    axios
+      .get(
+        'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false'
+      )
+      .then((res) => {
+        setCoins(res.data);
+      })
+      .catch((error) => console.log(error));
+  }, []);
+
+  return <div className="App"></div>;
 }
 
 export default App;

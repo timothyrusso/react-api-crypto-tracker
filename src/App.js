a4755d4d5229e4980bc9e14f722c9ad4717bd634
timothyrusso <russotimothy@live.it>
2022-07-30 16:30:08 +0200

    feat: filteredCoins on the page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React, { useState, useEffect } from 'react';
 import './App.css';
 import axios from 'axios';
+import Coin from './Coin';
 
 function App() {
   const [coins, setCoins] = useState([]);
+  const [search, setSearch] = useState('');
 
   useEffect(() => {
     axios
@@ -16,18 +18,39 @@ function App() {
       .catch((error) => console.log(error));
   }, []);
 
+  const handleChange = (event) => {
+    setSearch(event.target.value);
+  };
+
+  const filteredCoins = coins.filter((coin) =>
+    coin.name.toLowerCase().includes(search.toLocaleLowerCase())
+  );
+
   return (
     <div className="coin-app">
       <div className="coin-search">
         <h1 className="coin-text">Search a currency</h1>
         <form>
-          <imput
+          <input
             type="text"
             placeholder="Search"
             className="coin-input"
-          ></imput>
+            onChange={handleChange}
+          ></input>
         </form>
       </div>
+      {filteredCoins.map((coin) => {
+        return (
+          <Coin
+            key={coin.id}
+            name={coin.name}
+            image={coin.image}
+            symbol={coin.symbol}
+            price={coin.current_price}
+            volume={coin.market_cap}
+          />
+        );
+      })}
     </div>
   );
 }

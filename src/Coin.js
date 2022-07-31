e48256eb49e6bcd7041b6c1eeb2cb663d6b7ced5
timothyrusso <russotimothy@live.it>
2022-07-31 16:05:48 +0200

    feat: responsive design for the coins table

--- src/Coin.js
+++ src/Coin.js
@@ -13,12 +13,10 @@ const Coin = ({
   return (
     <div className="coin-container">
       <div className="coin-row">
-        <div className="coin">
+        <div className="coin-data">
           <img src={image} alt="crypto" />
           <h1>{name}</h1>
           <p className="coin-symbol">{symbol}</p>
-        </div>
-        <div className="coin-data">
           <p className="coin-price">${price}</p>
           <p className="coin-volume">${volume.toLocaleString()}</p>
           {priceChange < 0 ? (
